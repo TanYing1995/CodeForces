a1cabbfe642d8fa6072a06c4e3f1bc40239d34c7
TanYing1995 <694024566@qq.com>
2023-06-23 21:22:27 +0800

    460C

---
+++ src/CodeForce/BinarySearch/C460.java
@@ -0,0 +1,81 @@
+//package BinarySearch;
+
+import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class C460 {
+    static int[] nums;
+    static int n,m,w;
+    public static void main(String[] args) {
+        Kattio io = new Kattio();
+        //这是一个输入输出模板
+        n = io.nextInt();
+        m = io.nextInt();
+        w = io.nextInt();
+        nums = new int[n];
+        for(int i = 0; i < n; i++){ 
+            nums[i] = io.nextInt();
+        }
+        int l = 1,r = (int)1e9+100001;
+        //二分答案
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(check(mid)){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+            System.out.println(l + "  "+ r);
+        }
+        System.out.println(r);
+        io.close();
+    }
+
+    //将最小数字变成k所需的操作次数
+    static boolean check(int k){
+        int[] diff = new int[nums.length];
+        diff[0] = nums[0];
+        for(int i = 1; i < diff.length; i++){
+            diff[i] = nums[i]-nums[i-1];
+        }
+        int v = 0;
+        long cnt = 0;
+        for(int i = 0; i < n; i++){
+            v += diff[i];
+            if(v < k){
+                if(i+w-1 < n) diff[i+w-1] -= (k-v);
+                cnt += (k-v);
+            }
+        }
+        return cnt <= m;
+    }
+
+    static class Kattio extends PrintWriter {
+        private BufferedReader r;
+        private StringTokenizer st;
+        // 标准 IO
+        public Kattio() { this(System.in, System.out); }
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+        // 文件 IO
+        public Kattio(String intput, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(intput));
+        }
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(r.readLine());
+                return st.nextToken();
+            } catch (Exception e) {}
+            return null;
+        }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public double nextDouble() { return Double.parseDouble(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
