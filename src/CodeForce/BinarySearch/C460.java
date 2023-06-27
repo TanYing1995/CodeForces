705989e91ced3d23ef5a387509837cd76c4f4283
TanYing1995 <694024566@qq.com>
2023-06-27 16:30:46 +0800

    C1081

--- src/CodeForce/BinarySearch/C460.java
+++ src/CodeForce/BinarySearch/C460.java
@@ -1,4 +1,4 @@
-package CodeForce.BinarySearch;
+//package BinarySearch;
 
 import java.util.*;
 import java.io.*;
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
  */
 public class C460 {
     static int[] nums;
-    static int n,m,w,mi;
+    static int n,m,w;
     public static void main(String[] args) {
         Kattio io = new Kattio();
         //这是一个输入输出模板
@@ -20,12 +20,6 @@ public class C460 {
         m = io.nextInt();
         w = io.nextInt();
         nums = new int[n];
-
-        mi = (int)1e9;
-        for(int i = 0; i < n; i++){ 
-            nums[i] = io.nextInt();
-            mi = Math.min(mi,nums[i]);
-        }
         int mi = (int)1e9;
         for(int i = 0; i < n; i++){ 
             nums[i] = io.nextInt();
@@ -36,7 +30,7 @@ public class C460 {
             io.close();
             return;
         }
-        int l = mi,r = (int)1e9+100001;
+        int l = 1,r = (int)1e9+100001;
         //二分答案
         while(l <= r){
             int mid = l+(r-l)/2;
@@ -45,10 +39,9 @@ public class C460 {
             }else{
                 r = mid-1;
             }
-            //System.out.println(l + "  "+ r);
+            System.out.println(l + "  "+ r);
         }
         System.out.println(r);
-        //System.out.println(check(499));
         io.close();
     }
 
@@ -58,20 +51,18 @@ public class C460 {
         diff[0] = nums[0];
         for(int i = 1; i < diff.length; i++){
             diff[i] = nums[i]-nums[i-1];
-            //System.out.print(diff[i] + " ");
         }
-        //System.out.println();
-        long v = 0;
+        int v = 0;
         long cnt = 0;
         for(int i = 0; i < n; i++){
             v += diff[i];
             if(v < k){
-                if(i+w < n) diff[i+w] -= (k-v);
-                cnt = cnt +(k-v);
-                v = k;//增加当前值到k
+                if(i+w-1 < n) diff[i+w-1] -= (k-v);
+                cnt += (k-v);
             }
         }
-         return cnt <= m;
+        
+        return cnt <= m;
     }
 
     static class Kattio extends PrintWriter {
---
+++ src/CodeForce/Dp/C1081.java
@@ -0,0 +1,63 @@
+package CodeForce.Dp;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * https://codeforces.com/problemset/problem/1081/C
+ * DP 1500
+ */
+public class C1081 {
+    static final int MOD = 998244353;
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio();
+        // Template
+        int n = io.nextInt();
+        int m = io.nextInt();
+        int k = io.nextInt();
+        long[][] dp = new long[n][k+1];
+
+        for(int i = 0; i < n ; i++){
+            dp[i][0] = m;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= k; j++){
+                dp[i][j] = (dp[i-1][j]+(dp[i-1][j-1]%MOD*(m-1)%MOD)%MOD + MOD)% MOD;
+            }
+        }
+        System.out.println(dp[n-1][k]);
+        io.close();
+    }
+
+    static class Kattio extends PrintWriter {
+        private BufferedReader r;
+        private StringTokenizer st;
+        // 标准 IO
+        public Kattio() { this(System.in, System.out); }
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+        // 文件 IO
+        public Kattio(String intput, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(intput));
+        }
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(r.readLine());
+                return st.nextToken();
+            } catch (Exception e) {}
+            return null;
+        }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public double nextDouble() { return Double.parseDouble(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
